33179d1e0c02434dc725df4c23914f83b200eed1
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-08-11 21:19:03 +0200

    Add fr.readFile for sending an html files, status codes for cases & redirect

--- index.js
+++ index.js
@@ -4,19 +4,39 @@ import fs from 'node:fs';
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
 
-  let path = './views';
+  let path = './views/';
   switch (req.url) {
     case '/':
       path += 'index.html';
+      res.statusCode = 200;
       break;
     case '/about':
       path += 'about.html';
+      res.statusCode = 200;
       break;
     case '/contact-me':
       path += 'contact-me.html';
+      res.statusCode = 200;
       break;
+    // Redirect
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      return;
     default:
       path += '404.html';
+      res.statusCode = 404;
       break;
   }
+
+  // Send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
