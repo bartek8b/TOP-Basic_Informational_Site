d1daaf6d33b09052f18217b96a62c48dc183eae5
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-08-11 21:03:36 +0200

    Set switch cases for views

---
+++ index.js
@@ -0,0 +1,22 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    case '/contact-me':
+      path += 'contact-me.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
+});
