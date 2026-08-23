65d0eedfaca8726dbad8a2aa424bf719d6c3c922
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-08-23 11:26:10 +0200

    Reorganize order

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.static('public'));
-
 // Dynamic PORT - allow external connections
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening for requests on port ${PORT}`);
+});
+
+// Static .css file service
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('/views/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.send('/views/about.html');
+});
+
+app.get('/contact-me', (req, res) => {
+  res.send('/views/contact-me.html');
+});
+
+app.get('/', (req, res) => {
+  res.send('/views/index.html');
 });
